f43189bdbc8bfdfb310a6dfbd9ef9e5b1ad51291
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2022-02-15 09:23:39 -0800

    finish home_screen.dart

--- lib/screens/cart_screen/cart_screen.dart
+++ lib/screens/cart_screen/cart_screen.dart
@@ -39,8 +39,10 @@ class CartScreen extends StatelessWidget {
                       image: "assets/images/kit.jpg",
                       decrease: () {},
                       increase: () {},
+                      clear: () {},
                     );
                   }),
+              K.sizedBoxH,
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: const [
